0e5a3261df2f22bc843d58eaae4051b7ce0d5121
Kaue-LS <kaue.ls0506@gmail.com>
2023-05-12 21:19:06 -0300

    feat: add status module

--- src/components/cardList/index.tsx
+++ src/components/cardList/index.tsx
@@ -4,21 +4,37 @@ import { UseContext } from '../context/getContext'
 export default function CardList() {
   const { useDataContext } = UseContext()
   const { character } = useDataContext
+
+  // eslint-disable-next-line
+  console.log({ character })
   return (
-    <section>
-      <div>
+    <section className='e-cardList'>
+      <div className='e-cardList__container'>
         {character.map((item) => {
           return (
-            <div key={item.id}>
-              <div>
-                <img alt={item.name} loading={'lazy'} src={item.image} />
+            <div className='e-cardList__container__card' key={item.id}>
+              <div className='e-cardList__container__card__imageContainer'>
+                <img alt={item.name} width={200} height={200} loading={'lazy'} src={item.image} />
               </div>
-              <label>Name:</label>
-              <span>{item.name}</span>
-              <label>Gender:</label>
-              <span>{item.gender}</span>
-              <label>Type:</label>
-              <span>{item.type}</span>
+              <div className='e-cardList__container__card__info'>
+                <div className={'e-cardList__container__card__info__status'}>
+                  <span
+                    className={`e-cardList__container__card__info__status__${item.status}`}
+                  ></span>
+                </div>
+                <div className='e-cardList__container__card__info__label'>
+                  <label>Name:</label>
+                  <span>{item.name}</span>
+                </div>
+                <div className='e-cardList__container__card__info__label'>
+                  <label>Gender:</label>
+                  <span>{item.gender}</span>
+                </div>
+                <div className='e-cardList__container__card__info__label'>
+                  <label>Specie:</label>
+                  <span>{item.species}</span>
+                </div>
+              </div>
             </div>
           )
         })}
