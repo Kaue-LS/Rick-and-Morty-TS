0c6e6414f177613f0e5c870c53c9e230b77e6b15
Kaue-LS <kaue.ls0506@gmail.com>
2023-05-14 12:25:37 -0300

    refactor: add configuration on build

--- src/components/navBar/index.tsx
+++ src/components/navBar/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import SwitchDevice from '../device/switchDevice'
-import '../styles/css/styles.css'
 import NavBarDesktop from './navBarDesktop'
 import NavBarMobile from './navBarMobile'
 export default function NavBar() {
