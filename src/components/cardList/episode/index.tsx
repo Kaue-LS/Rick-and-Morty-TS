6700e8ea27350c1c1c17a44245400f4496ccf146
KaueLS <kaue.ls0506@gmail.com>
2023-08-31 17:34:09 -0300

    style: episodeList

--- src/components/cardList/episode/index.tsx
+++ src/components/cardList/episode/index.tsx
@@ -8,9 +8,14 @@ export default function EpisodeList() {
         <div className='e-episodeList__container'>
             {data.episodes.map((item) => (
                 <div key={item.id} className='e-episodeList__container__card' >
-                    <span>#{item.id}</span>
-                    <h3 >{item.name}</h3>
-                    <p>{item.air_date}</p>
+                    <div className='e-episodeList__container__card__name'>
+                        <span>#{item.id}</span>
+                        <h3 >{item.name}</h3>
+                    </div>
+                    <div className='e-episodeList__container__card__info'>
+                        <span>{item.episode}</span>
+                        <h4>{item.air_date}</h4>
+                    </div>
                 </div>
             ))}
         </div>
